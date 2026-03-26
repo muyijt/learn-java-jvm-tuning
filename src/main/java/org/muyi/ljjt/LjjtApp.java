d6599569ce3c98986193b47635b7af1fefa87a65
muyijt0 <muyijt0@163.com>
2026-03-26 21:21:22 +0800

    Initial project setup with Spring Boot 3.5.9, Java 21, and Maven

---
+++ src/main/java/org/muyi/ljjt/LjjtApp.java
@@ -0,0 +1,11 @@
+package org.muyi.ljjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjjtApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LjjtApp.class,args);
+    }
+}
